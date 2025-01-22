f7e7f91414fadfcae1859b8cd9de7b110d88d723
Nakorn K <nakorn.kitk@gmail.com>
2025-01-22 19:48:03 +0700

    adjust z-index

--- src/components/FixedButton.js
+++ src/components/FixedButton.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react'
 import { IoIosArrowUp } from "react-icons/io";
-import { FaShareAltSquare, FaRegAddressCard, FaFacebookSquare,FaYoutube, FaLine, FaInstagram } from "react-icons/fa";
+import { FaShareAltSquare, FaRegAddressCard, FaFacebookSquare, FaYoutube, FaLine, FaInstagram } from "react-icons/fa";
 import { MdOutlineEmail } from "react-icons/md";
 
 function FixedButton() {
@@ -47,17 +47,17 @@ function FixedButton() {
                     <FaRegAddressCard className="" />
                 </button>
                 <a href="https://page.line.me/156vctty?openQrModal=true"
-                    className={`fixed left-[20px] h-[35px] w-[35px] bg-[#E2B22C] text-white flex items-center justify-center rounded-full shadow-xl hover:bg-white hover:text-[#42189F] hover:border hover:border-[#42189F] transition-all duration-300 ${isContactActive ? 'bottom-[115px]' : 'bottom-[65px]'}`}
+                    className={`z-20 fixed left-[20px] h-[35px] w-[35px] bg-[#E2B22C] text-white flex items-center justify-center rounded-full shadow-xl hover:bg-white hover:text-[#42189F] hover:border hover:border-[#42189F] transition-all duration-300 ${isContactActive ? 'bottom-[115px]' : 'bottom-[65px]'}`}
                 >
                     <FaLine />
                 </a>
                 <a href="https://www.facebook.com/profile.php?id=61555700229121"
-                    className={`fixed left-[20px] h-[35px] w-[35px] bg-[#E2B22C] text-white flex items-center justify-center rounded-full shadow-xl hover:bg-white hover:text-[#42189F] hover:border hover:border-[#42189F] transition-all duration-300 ${isContactActive ? 'bottom-[165px]' : 'bottom-[65px]'}`}
+                    className={`z-20 fixed left-[20px] h-[35px] w-[35px] bg-[#E2B22C] text-white flex items-center justify-center rounded-full shadow-xl hover:bg-white hover:text-[#42189F] hover:border hover:border-[#42189F] transition-all duration-300 ${isContactActive ? 'bottom-[165px]' : 'bottom-[65px]'}`}
                 >
                     <FaFacebookSquare className="" />
                 </a>
                 <a href="mailto:janenyrilon_jingwei@hotmail.com"
-                    className={`fixed left-[20px] h-[35px] w-[35px] bg-[#E2B22C] text-white flex items-center justify-center rounded-full shadow-xl hover:bg-white hover:text-[#42189F] hover:border hover:border-[#42189F] transition-all duration-300 ${isContactActive ? 'bottom-[215px]' : 'bottom-[65px]'}`}
+                    className={`z-20 fixed left-[20px] h-[35px] w-[35px] bg-[#E2B22C] text-white flex items-center justify-center rounded-full shadow-xl hover:bg-white hover:text-[#42189F] hover:border hover:border-[#42189F] transition-all duration-300 ${isContactActive ? 'bottom-[215px]' : 'bottom-[65px]'}`}
                 >
                     <MdOutlineEmail className="" />
                 </a>
@@ -73,22 +73,22 @@ function FixedButton() {
                     <FaShareAltSquare className="" />
                 </button>
                 <a href="https://page.line.me/156vctty?openQrModal=true"
-                    className={`fixed bottom-[15px] h-[35px] w-[35px] bg-[#E2B22C] text-white flex items-center justify-center rounded-xl shadow-xl hover:bg-white hover:text-[#42189F] hover:border hover:border-[#42189F] transition-all duration-300 ${isSocialActive ? 'left-[70px]' : 'left-[20px]'}`}
+                    className={`z-20 fixed bottom-[15px] h-[35px] w-[35px] bg-[#E2B22C] text-white flex items-center justify-center rounded-xl shadow-xl hover:bg-white hover:text-[#42189F] hover:border hover:border-[#42189F] transition-all duration-300 ${isSocialActive ? 'left-[70px]' : 'left-[20px]'}`}
                 >
                     <FaLine />
                 </a>
                 <a href="https://www.facebook.com/profile.php?id=61555700229121"
-                    className={`fixed bottom-[15px] h-[35px] w-[35px] bg-[#E2B22C] text-white flex items-center justify-center rounded-xl shadow-xl hover:bg-white hover:text-[#42189F] hover:border hover:border-[#42189F] transition-all duration-300 ${isSocialActive ? 'left-[120px]' : 'left-[20px]'}`}
+                    className={`z-20 fixed bottom-[15px] h-[35px] w-[35px] bg-[#E2B22C] text-white flex items-center justify-center rounded-xl shadow-xl hover:bg-white hover:text-[#42189F] hover:border hover:border-[#42189F] transition-all duration-300 ${isSocialActive ? 'left-[120px]' : 'left-[20px]'}`}
                 >
                     <FaFacebookSquare className="" />
                 </a>
                 <a href="https://www.instagram.com/rilon_thailand/"
-                    className={`fixed bottom-[15px] h-[35px] w-[35px] bg-[#E2B22C] text-white flex items-center justify-center rounded-xl shadow-xl hover:bg-white hover:text-[#42189F] hover:border hover:border-[#42189F] transition-all duration-300 ${isSocialActive ? 'left-[170px]' : 'left-[20px]'}`}
+                    className={`z-20 fixed bottom-[15px] h-[35px] w-[35px] bg-[#E2B22C] text-white flex items-center justify-center rounded-xl shadow-xl hover:bg-white hover:text-[#42189F] hover:border hover:border-[#42189F] transition-all duration-300 ${isSocialActive ? 'left-[170px]' : 'left-[20px]'}`}
                 >
                     <FaInstagram className="" />
                 </a>
                 <a href="https://www.youtube.com/@rilon_thailand"
-                    className={`fixed bottom-[15px] h-[35px] w-[35px] bg-[#E2B22C] text-white flex items-center justify-center rounded-xl shadow-xl hover:bg-white hover:text-[#42189F] hover:border hover:border-[#42189F] transition-all duration-300 ${isSocialActive ? 'left-[220px]' : 'left-[20px]'}`}
+                    className={`z-20 fixed bottom-[15px] h-[35px] w-[35px] bg-[#E2B22C] text-white flex items-center justify-center rounded-xl shadow-xl hover:bg-white hover:text-[#42189F] hover:border hover:border-[#42189F] transition-all duration-300 ${isSocialActive ? 'left-[220px]' : 'left-[20px]'}`}
                 >
                     <FaYoutube className="" />
                 </a>
